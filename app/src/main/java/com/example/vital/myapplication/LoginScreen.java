5474f1ddaec76c7922ff87a9b4885a52f67a9a47
werton14 <qwerty191498@gmail.com>
2016-12-18 20:00:46 +0300

    Залил на гитхаб

---
+++ app/src/main/java/com/example/vital/myapplication/LoginScreen.java
@@ -0,0 +1,20 @@
+package com.example.vital.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by vital on 11.12.2016.
+ */
+
+public class LoginScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+}
---
+++ app/src/main/java/com/example/vital/myapplication/StartScreen.java
@@ -0,0 +1,27 @@
+package com.example.vital.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+
+        Button logIn = (Button) findViewById(R.id.log_in);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginScreen.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
