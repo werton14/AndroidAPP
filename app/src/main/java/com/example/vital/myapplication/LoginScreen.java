3720231010b4218205ba5a535c69ffd7170ee05f
werton14 <qwerty191498@gmail.com>
2017-02-11 23:48:50 +0300

    Add sign in

--- app/src/main/java/com/example/vital/myapplication/LoginScreen.java
+++ app/src/main/java/com/example/vital/myapplication/LoginScreen.java
@@ -2,29 +2,71 @@ package com.example.vital.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by vital on 11.12.2016.
  */
 
 public class LoginScreen extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        Button singIn = (Button) findViewById(R.id.log_in);
+        mAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener(){
 
-        singIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Intent intent = new Intent(getApplicationContext(), Choose_screen.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        final Button signIn = (Button) findViewById(R.id.log_in);
+        final EditText email = (EditText) findViewById(R.id.Email);
+        final EditText password = (EditText) findViewById(R.id.Password);
+
+
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Choose_screen.class);
-                startActivity(intent);
+                signInWithEmailAndPassword(email.getText().toString(), password.getText().toString());
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener != null){
+            mAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+
+    private void signInWithEmailAndPassword(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password);
     }
 }
