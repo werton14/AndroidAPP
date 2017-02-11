8326e5edbdef3687471a722523b1f5b7b3952f51
Defaault <narutik.temik@mail.ru>
2017-02-12 01:27:28 +0200

    New style first_fragment

--- app/src/main/java/com/example/vital/myapplication/LoginScreen.java
+++ app/src/main/java/com/example/vital/myapplication/LoginScreen.java
@@ -84,7 +84,7 @@ public class LoginScreen extends AppCompatActivity {
             @Override
             public void onFailure(@NonNull Exception e) {
 
-            }
-        })
+        }
+        });
     }
 }
