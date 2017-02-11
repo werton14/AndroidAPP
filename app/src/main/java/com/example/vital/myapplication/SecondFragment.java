ae410ee440ce6e128c278db6a24e02dc5c23a3a2
werton14 <qwerty191498@gmail.com>
2017-02-12 02:49:52 +0300

    Create SecondFragment class

---
+++ app/src/main/java/com/example/vital/myapplication/SecondFragment.java
@@ -0,0 +1,21 @@
+package com.example.vital.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by qwert on 12.02.2017.
+ */
+
+public class SecondFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.second_fragment, container, false);
+    }
+}
--- app/src/main/java/com/example/vital/myapplication/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/vital/myapplication/SectionsPagerAdapter.java
@@ -17,14 +17,15 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int position) {
         switch (position){
-            case 0 : return  new FirstFragment();
-            case 1 : return  new CameraFragment();
+            case 0 : return  new SecondFragment();
+            case 1 : return  new FirstFragment();
+            case 2 : return  new CameraFragment();
         }
         return  null;
     }
 
     @Override
     public int getCount() {
-        return 2;
+        return 3;
     }
 }
