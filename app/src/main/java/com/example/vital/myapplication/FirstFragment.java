c03922d804d9da8e7f28e03b8282fe5a75d64762
werton14 <qwerty191498@gmail.com>
2017-02-13 01:27:06 +0300

    Modify SignupScreen class

--- app/src/main/java/com/example/vital/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/vital/myapplication/FirstFragment.java
@@ -1,19 +1,30 @@
 package com.example.vital.myapplication;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class FirstFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.first_fragment, container, false);
+        final View rootView = inflater.inflate(R.layout.first_fragment, container, false);
+        Button tempButton = (Button) rootView.findViewById(R.id.button_2);
+
+        tempButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MainPage.class);
+                startActivity(intent);
+            }
+        });
+        return  rootView;
     }
 }
 
