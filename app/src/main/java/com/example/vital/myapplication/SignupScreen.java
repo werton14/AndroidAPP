26120c10e3bdee22da7e95704dbff6ae9873e14f
werton14 <qwerty191498@gmail.com>
2017-02-12 17:46:08 +0300

    Modify SignupScreen class

--- app/src/main/java/com/example/vital/myapplication/SignupScreen.java
+++ app/src/main/java/com/example/vital/myapplication/SignupScreen.java
@@ -1,13 +1,18 @@
 package com.example.vital.myapplication;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,11 +20,20 @@ public class SignupScreen extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private EditText editNickname;
+    private EditText editEmail;
+    private EditText editPassword;
+    private EditText editPasswordRepeat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen);
+
+        editNickname = (EditText) findViewById(R.id.Nickname);
+        editEmail = (EditText) findViewById(R.id.email);
+        editPassword = (EditText) findViewById(R.id.password);
+        editPasswordRepeat = (EditText) findViewById(R.id.repeat_password);
 
         mAuth = FirebaseAuth.getInstance();
         authStateListener = new FirebaseAuth.AuthStateListener(){
@@ -41,14 +55,38 @@ public class SignupScreen extends AppCompatActivity {
         singInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString());
+                if (TextUtils.isEmpty(editNickname.getText().toString())) {
+                    editNickname.setError("This field cannot be empty!");
+                }
+                if (TextUtils.isEmpty(editEmail.getText().toString())) {
+                    editEmail.setError("This field cannot be empty!");
+                }
+                if (TextUtils.isEmpty(editPassword.getText().toString())) {
+                    editPassword.setError("This field cannot be empty!");
+                }
+                if (TextUtils.isEmpty(editPasswordRepeat.getText().toString())) {
+                    editPasswordRepeat.setError("This field cannot be empty!");
+                }if(editPassword.getText().toString().compareTo(editPasswordRepeat.getText().toString()) != 0){
+                    Log.w("EP", editPassword.getText().toString());
+                    Log.w("EPR", editPasswordRepeat.getText().toString());
+                    Toast.makeText(getApplicationContext(), "Passwords do not match!", Toast.LENGTH_LONG).show();
+                }if(!TextUtils.isEmpty(editNickname.getText().toString()) && !TextUtils.isEmpty(editEmail.getText().toString())
+                        && !TextUtils.isEmpty(editPassword.getText().toString()) && !TextUtils.isEmpty(editPasswordRepeat.getText().toString())
+                        && editPassword.getText().toString().compareTo(editPasswordRepeat.getText().toString()) == 0) {
+                    createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString());
+                }
             }
         });
     }
 
     private void createUserWithEmailAndPassword(String email, String password){
 
-        mAuth.createUserWithEmailAndPassword(email, password);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
