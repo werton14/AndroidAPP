9e0a74b5f4121826f3666928f7280a3503ec9908
werton14 <qwerty191498@gmail.com>
2017-02-15 01:09:42 +0300

    add start camera on the 3 fragment

--- app/src/main/java/com/example/vital/myapplication/Choose_screen.java
+++ app/src/main/java/com/example/vital/myapplication/Choose_screen.java
@@ -1,8 +1,11 @@
 package com.example.vital.myapplication;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +16,7 @@ public class Choose_screen extends AppCompatActivity {
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     private ViewPager mViewPager;
+    private ViewPager.OnPageChangeListener onPageChangeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +28,36 @@ public class Choose_screen extends AppCompatActivity {
 
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        onPageChangeListener = new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                            }
+
+            }
 
             @Override
             public void onPageSelected(int position) {
-
+                if(position == 2) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivity(intent);
+                }
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
 
             }
-        });
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mViewPager.setOnPageChangeListener(onPageChangeListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mViewPager.removeOnPageChangeListener(onPageChangeListener);
     }
 }
