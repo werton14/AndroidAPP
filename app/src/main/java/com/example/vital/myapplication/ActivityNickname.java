9eed18e2200d667d6452cfdea9807706406281a9
werton14 <qwerty191498@gmail.com>
2017-02-17 00:41:53 +0300

    refactor 2

--- app/src/main/java/com/example/vital/myapplication/ActivityNickname.java
+++ app/src/main/java/com/example/vital/myapplication/ActivityNickname.java
@@ -3,11 +3,16 @@ package com.example.vital.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseUser;
+
 public class ActivityNickname extends AppCompatActivity {
+
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitynickname);
+
     }
 }
