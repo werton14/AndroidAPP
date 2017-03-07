6f4ba4911c94100b60dab5c7ea5ed2fa279e1820
werton14 <qwerty191498@gmail.com>
2017-03-07 12:13:40 +0300

    modify nickname class

--- app/src/main/java/com/example/vital/myapplication/ActivityNickname.java
+++ app/src/main/java/com/example/vital/myapplication/ActivityNickname.java
@@ -1,37 +1,46 @@
 package com.example.vital.myapplication;
 
+import android.support.v4.print.PrintHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class ActivityNickname extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private UserProfileChangeRequest.Builder userProfileChangeRequest;
+
+    private EditText firstNameEdit;
+    private EditText secondNameEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activitynickname);
+        setContentView(R.layout.activityname);
 
-        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        firstNameEdit = (EditText) findViewById(R.id.firstname);
+        String firstName = firstNameEdit.getText().toString();
+
+        secondNameEdit = (EditText) findViewById(R.id.secondname);
+        String secondName = secondNameEdit.getText().toString();
+
+        String nickName = firstName + ' ' + secondName;
+
+        userProfileChangeRequest = new UserProfileChangeRequest.Builder();
+        userProfileChangeRequest.setDisplayName(nickName);
 
         Button finish = (Button) findViewById(R.id.finish);
         final EditText nickname = (EditText) findViewById(R.id.nickname);
 
-        finish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                databaseReference.setValue(nickname.getText().toString());
-                DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("nickname");
-                ref.setValue("Nilson");
-            }
-        });
 
 
     }
