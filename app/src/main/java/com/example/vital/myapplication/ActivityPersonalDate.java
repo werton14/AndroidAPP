899d9c4a572fa9160591fedf4d1f13f7a7a7ecc6
Defaault <narutik.temik@mail.ru>
2017-04-02 02:25:12 +0300

    Много чего поменял а что не помню

--- app/src/main/java/com/example/vital/myapplication/ActivityPersonalDate.java
+++ app/src/main/java/com/example/vital/myapplication/ActivityPersonalDate.java
@@ -2,12 +2,24 @@ package com.example.vital.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class ActivityPersonalDate extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_date);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar  = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.inflateMenu(R.menu.menu_choose_screen);
+
     }
 }
