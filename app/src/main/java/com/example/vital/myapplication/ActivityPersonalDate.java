68ffae26d04e3f9d83b2c7f41b5f65299525b6b7
Defaault <narutik.temik@mail.ru>
2017-04-02 00:23:26 +0300

    Много чего поменял а что не помню

---
+++ app/src/main/java/com/example/vital/myapplication/ActivityPersonalDate.java
@@ -0,0 +1,13 @@
+package com.example.vital.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityPersonalDate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_date);
+    }
+}
