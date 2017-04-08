9e78f4830ed9960a2bff07dcb221cdf6b9d8c60e
Defaault <narutik.temik@mail.ru>
2017-04-09 00:11:57 +0300

    add bottom navigation

--- app/src/main/java/com/example/vital/myapplication/ActivityScroll.java
+++ app/src/main/java/com/example/vital/myapplication/ActivityScroll.java
@@ -2,6 +2,8 @@ package com.example.vital.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -14,6 +16,9 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
 
 public class ActivityScroll extends AppCompatActivity {
 
@@ -21,7 +26,7 @@ public class ActivityScroll extends AppCompatActivity {
     private LinearLayout scrollLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activityscroll);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -31,23 +36,34 @@ public class ActivityScroll extends AppCompatActivity {
         scrollLayout = (LinearLayout) findViewById(R.id.scrollLayout);
         scrollView = (InteractiveScrollView) findViewById(R.id.scrollview);
 
-        addNewPhoto();
-        addNewPhoto();
-
-        scrollView.setOnBottomReachedListener(new InteractiveScrollView.OnBottomReachedListener() {
+        /*BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener(){
             @Override
-            public void onBottomReached() {
+                public void OnTabSelect(@IdRes int tabId) {                     хэштэг виталя доделай
+                if (tabId == R.id.tab_home){
+
+                }
+            }
+                                         }*/
+
+
+       /* addNewPhoto();
+        addNewPhoto();*/
+
+       /* scrollView.setOnBottomReachedListener(new InteractiveScrollView.OnBottomReachedListener() {
+            @Override
+            public void onBottomReached() {         Мс курага
                 addNewPhoto();
             }
-        });
+        });*/
 
     }
 
-    private  void addNewPhoto(){
+    /*private  void addNewPhoto(){
         ViewGroup container = null;
         Bundle instance = null;
         scrollLayout.addView(new ActivityPrototypeScroll().onCreateView(getLayoutInflater(), container, instance));
-    }
+    }*/
 
 }
 
---
+++ app/src/main/java/com/example/vital/myapplication/MyBottomNavigationView.java
@@ -0,0 +1,5 @@
+package com.example.vital.myapplication;
+
+
+public class MyBottomNavigationView {
+}
