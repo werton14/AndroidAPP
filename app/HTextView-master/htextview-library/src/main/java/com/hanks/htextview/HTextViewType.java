736cc4c4011bb32cf046ddd29e88e0971155663c
Defaault <narutik.temik@mail.ru>
2017-06-04 16:22:58 +0300

    delete some files

--- app/HTextView-master/htextview-library/src/main/java/com/hanks/htextview/HTextViewType.java
+++
@@ -1,8 +0,0 @@
-package com.hanks.htextview;
-
-/**
- * Created by hanks on 15-12-14.
- */
-public enum HTextViewType {
-    SCALE, EVAPORATE, FALL, PIXELATE, ANVIL, SPARKLE, LINE, TYPER, RAINBOW
-}
