144e09991f6579a5590c391af838a621f99ea65d
Defaault <narutik.temik@mail.ru>
2017-06-12 18:16:24 +0300

    added progress bar

--- app/src/main/java/com/example/vital/myapplication/ImageAdapter.java
+++ app/src/main/java/com/example/vital/myapplication/ImageAdapter.java
@@ -23,12 +23,29 @@ import java.util.List;
  * Created by werton on 03.06.17.
  */
 
-public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
+public class ImageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
     private List<Image> mImage;
     private List<User> mUser;
     private Context context;
     private int windowWidth;
+    private static final int VIEW_TYPE_MOVE = 1;
+    private static final int VIEW_TYPE_LOAD = 2;
+
+    @Override
+    public int getItemViewType(int position){
+        int viewType = VIEW_TYPE_MOVE;
+        Log.w("spy", String.valueOf(mUser.size()-1));
+        Log.w("spy", String.valueOf(position));
+        if(position < mUser.size()){
+            viewType = VIEW_TYPE_MOVE;
+        }
+        if (position == mUser.size() - 1){
+            Log.w("bugaga", String.valueOf(position));
+            viewType = VIEW_TYPE_LOAD;
+        }
+        return viewType;
+    }
 
     public ImageAdapter(Context context, List<Image> images, List<User> users){
         this.mImage = images;
@@ -40,48 +57,64 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
 
-        View imageView = inflater.inflate(R.layout.image, parent, false);
+        switch (viewType) {
+            case VIEW_TYPE_MOVE:
+                return new ViewHolder(inflater.inflate(R.layout.image,parent,false));
 
-        ViewHolder viewHolder = new ViewHolder(imageView);
+            case VIEW_TYPE_LOAD:
+                return new LoadHolder(inflater.inflate(R.layout.progress_bar,parent,false));
+        }
+        return null;
 
-        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Log.w("BindViewHolder", String.valueOf(position));
-        Image image = mImage.get(position);
-        User user = mUser.get(position);
-        FirebaseInfo firebaseInfo = FirebaseInfo.getInstance();
+    public void onBindViewHolder(RecyclerView.ViewHolder h, int position) {
 
-        final ImageView competitiveImageView = holder.competitiveImageView;
-        int currentHeight = (windowWidth * image.getHeight()) / image.getWidth();
-        competitiveImageView.setMinimumHeight(currentHeight);
+        Log.w("plaha",String.valueOf(h instanceof ViewHolder));
+        Log.w("plaha",String.valueOf(h instanceof LoadHolder));
 
-        firebaseInfo.getImagesSReference().child(image.getCompetitiveImageFileName()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                Picasso.with(context).load(uri)
-                        .resize(competitiveImageView.getWidth(), competitiveImageView.getHeight()).into(competitiveImageView);
-            }
-        });
-        final ImageButton profileImageButton = holder.profileImageButton;
-        firebaseInfo.getProfileImagesSReference().child(user.getProfileImageFileName()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                Picasso.with(context).load(uri)
-                        .resize(profileImageButton.getWidth(), profileImageButton.getHeight()).into(profileImageButton);
-            }
-        });
 
-        TextView nicknameTextView = holder.nicknameTextView;
-        nicknameTextView.setText(user.getNickname());
+        if (h instanceof ViewHolder){
+            ViewHolder holder = (ViewHolder) h;
+            Log.w("BindViewHolder", String.valueOf(position));
+            Image image = mImage.get(position);
+            User user = mUser.get(position);
+            FirebaseInfo firebaseInfo = FirebaseInfo.getInstance();
 
-        TextView likeTextView = holder.likeTextView;
-        likeTextView.setText(String.valueOf(image.getLikeCount()));
+            final ImageView competitiveImageView =  holder.competitiveImageView;
+            int currentHeight = (windowWidth * image.getHeight()) / image.getWidth();
+            competitiveImageView.setMinimumHeight(currentHeight);
+
+            firebaseInfo.getImagesSReference().child(image.getCompetitiveImageFileName()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Picasso.with(context).load(uri)
+                            .resize(competitiveImageView.getWidth(), competitiveImageView.getHeight()).into(competitiveImageView);
+                }
+            });
+            final ImageButton profileImageButton = holder.profileImageButton;
+            firebaseInfo.getProfileImagesSReference().child(user.getProfileImageFileName()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Picasso.with(context).load(uri)
+                            .resize(profileImageButton.getWidth(), profileImageButton.getHeight()).into(profileImageButton);
+                }
+            });
+
+            TextView nicknameTextView = holder.nicknameTextView;
+            nicknameTextView.setText(user.getNickname());
+
+            TextView likeTextView = holder.likeTextView;
+            likeTextView.setText(String.valueOf(image.getLikeCount()));
+        }else if (h instanceof LoadHolder){
+
+            Log.w("pisun",String.valueOf(position));
+        }
+
     }
 
     @Override
@@ -106,6 +139,12 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
             competitiveImageView = (ImageView) itemView.findViewById(R.id.competitive_image_view);
             nicknameTextView = (TextView) itemView.findViewById(R.id.nickname_text_view);
             likeTextView = (TextView) itemView.findViewById(R.id.like_text_view);
+        }
+    }
+
+    private class LoadHolder extends RecyclerView.ViewHolder {
+        public LoadHolder(View itemView) {
+            super(itemView);
         }
     }
 }
