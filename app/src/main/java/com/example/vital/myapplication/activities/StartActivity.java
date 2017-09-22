1364d549fc0d3829ecfe2c3a439e30a6c9041ea0
Defaault <narutik.temik@mail.ru>
2017-09-22 18:36:52 +0300

    added toolbar in signup, in and activityname

--- app/src/main/java/com/example/vital/myapplication/activities/StartActivity.java
+++ app/src/main/java/com/example/vital/myapplication/activities/StartActivity.java
@@ -5,9 +5,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -19,33 +19,10 @@ public class StartActivity extends AppCompatActivity {
 
     FirebaseInfo firebaseInfo;
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public static void setStatusBarGradient(Activity activity) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = activity.getWindow();
-            Drawable background = ResourcesCompat
-                    .getDrawable(activity.getResources(), R.drawable.background_gradient, null);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-//            window.setStatusBarColor(ResourcesCompat
-//                    .getColor(activity.getResources(), R.color.backgroungforpicture1, null));
-//            window.setNavigationBarColor(ResourcesCompat
-//                    .getColor(activity.getResources(), R.color.clr_normal_left, null));
-            window.setBackgroundDrawable(background);
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        //Для градиента Statusbar
-        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow();
-            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        }*/
-
-        setStatusBarGradient(this);
-
         setContentView(R.layout.activitystart);
 
         firebaseInfo = FirebaseInfo.getInstance();
