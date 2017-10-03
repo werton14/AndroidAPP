f2aad9d9bd103127d8d5230fd25cc7ec86aecfa4
werton14 <v9i3t2a1l5y>
2017-10-03 21:13:26 +0300

    activity leaders complete 93%

--- app/src/main/java/com/example/vital/myapplication/FragmentCamera.java
+++ app/src/main/java/com/example/vital/myapplication/FragmentCamera.java
@@ -1,68 +1,25 @@
 package com.example.vital.myapplication;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.androidhiddencamera.CameraConfig;
-import com.androidhiddencamera.HiddenCameraFragment;
-import com.androidhiddencamera.config.CameraFacing;
-import com.androidhiddencamera.config.CameraImageFormat;
-import com.androidhiddencamera.config.CameraResolution;
-import com.androidhiddencamera.config.CameraRotation;
-import com.example.vital.myapplication.activities.ContainerUserFragmentsActivity;
-
-import java.io.File;
 
 
-public class FragmentCamera extends HiddenCameraFragment {
-
-    private CameraConfig mCameraConfig;
+public class FragmentCamera extends Fragment{
 
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragmentcamera, container, false);
         Button tempButton = (Button) rootView.findViewById(R.id.button2);
-        
 
-        /*tempButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), ContainerUserFragmentsActivity.class);
-                startActivity(intent);
-            }
-        });*/
-        mCameraConfig = new CameraConfig()
-                .getBuilder(getActivity())
-                .setCameraFacing(CameraFacing.FRONT_FACING_CAMERA)
-                .setCameraResolution(CameraResolution.MEDIUM_RESOLUTION)
-                .setImageFormat(CameraImageFormat.FORMAT_JPEG)
-                .setImageRotation(CameraRotation.ROTATION_270)
-                .build();
-        startCamera(mCameraConfig);
-        tempButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                takePicture();
-            }
-        });
+
         return  rootView;
     }
 
-    @Override
-    public void onImageCapture(@NonNull File imageFile) {
-
-    }
-
-    @Override
-    public void onCameraError(int errorCode) {
-
-    }
 }
 
