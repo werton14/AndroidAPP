5a355b39d47cfc1972503585194e6f8fb5a05163
werton14 <v9i3t2a1l5y>
2017-10-10 21:15:14 +0300

    camera v. 1

--- app/src/main/java/com/example/vital/myapplication/FragmentCamera.java
+++ app/src/main/java/com/example/vital/myapplication/FragmentCamera.java
@@ -1,24 +1,70 @@
 package com.example.vital.myapplication;
 
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class FragmentCamera extends Fragment{
+
+    private ImageSurfaceView mImageSurfaceView;
+    private Camera camera;
+
+    private FrameLayout cameraPreviewLayout;
 
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragmentcamera, container, false);
-        Button tempButton = (Button) rootView.findViewById(R.id.button2);
+        Button tempButton = (Button) rootView.findViewById(R.id.take_photo);
 
+        cameraPreviewLayout = (FrameLayout)rootView.findViewById(R.id.camera_preview);
+
+        camera = checkDeviceCamera();
+        mImageSurfaceView = new ImageSurfaceView(getContext(), camera);
+        cameraPreviewLayout.addView(mImageSurfaceView);
+
+        tempButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                camera.takePicture(null, null, pictureCallback);
+            }
+        });
 
         return  rootView;
+    }
+
+    private Camera checkDeviceCamera(){
+        Camera mCamera = null;
+        try {
+            mCamera = Camera.open();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mCamera;
+    }
+
+    Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            if(bitmap==null){
+                Toast.makeText(getContext(), "Captured image is empty", Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+    };
+
+    private Bitmap scaleDownBitmapImage(Bitmap bitmap, int newWidth, int newHeight){
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
+        return resizedBitmap;
     }
 
 }
---
+++ app/src/main/java/com/example/vital/myapplication/ImageSurfaceView.java
@@ -0,0 +1,49 @@
+package com.example.vital.myapplication;
+
+/**
+ * Created by werton on 03.10.17.
+ */
+
+import android.hardware.Camera;
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class ImageSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
+
+    public ImageSurfaceView(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setRotation(90);
+        camera.setParameters(parameters);
+        this.surfaceHolder = getHolder();
+        this.surfaceHolder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            this.camera.setPreviewDisplay(holder);
+            this.camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        this.camera.stopPreview();
+        this.camera.release();
+    }
+}
