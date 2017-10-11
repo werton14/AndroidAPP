6b3afaaf3570bef0e7a67419d52827bd392172f2
werton14 <v9i3t2a1l5y>
2017-10-11 20:15:59 +0300

    add google sample camera

--- app/src/main/java/com/example/vital/myapplication/FragmentCamera.java
+++ app/src/main/java/com/example/vital/myapplication/FragmentCamera.java
@@ -1,71 +1,129 @@
 package com.example.vital.myapplication;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Build;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-public class FragmentCamera extends Fragment{
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
-    private ImageSurfaceView mImageSurfaceView;
-    private Camera camera;
+public class FragmentCamera extends Fragment {
 
-    private FrameLayout cameraPreviewLayout;
+    private CameraManager cameraManager;
+    private SurfaceView surfaceView;
+    private String backCameraId;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragmentcamera, container, false);
         Button tempButton = (Button) rootView.findViewById(R.id.take_photo);
+        surfaceView = (SurfaceView) rootView.findViewById(R.id.camera_preview);
 
-        cameraPreviewLayout = (FrameLayout)rootView.findViewById(R.id.camera_preview);
+        cameraManager = rootView.getContext().getSystemService(CameraManager.class);
+        backCameraId = getBackCameraId();
+        CameraCharacteristics cameraCharacteristics = null;
+        try {
+            cameraCharacteristics = cameraManager.getCameraCharacteristics(backCameraId);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        StreamConfigurationMap configs = cameraCharacteristics
+                .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+        if (ActivityCompat.checkSelfPermission(rootView.getContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+        }
+        try {
+            cameraManager.openCamera(backCameraId, new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(@NonNull CameraDevice camera) {
+                    try {
+                        CaptureRequest.Builder captureRequest = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                        Surface surface = surfaceView.getHolder().getSurface();
+                        captureRequest.addTarget(surface);
+                        captureRequest.build();
+                        camera.createCaptureSession(Collections.singletonList(surface),
+                                new CameraCaptureSession.StateCallback() {
+                                    @Override
+                                    public void onConfigured(@NonNull CameraCaptureSession session) {
+                                    }
 
-        camera = checkDeviceCamera();
-        mImageSurfaceView = new ImageSurfaceView(getContext(), camera);
-        cameraPreviewLayout.addView(mImageSurfaceView);
+                                    @Override
+                                    public void onConfigureFailed(@NonNull CameraCaptureSession session) {
 
-        tempButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                camera.takePicture(null, null, pictureCallback);
-            }
-        });
+                                    }
+                                }, null);
+                        Log.w("onBuild", "hire me please");
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice camera) {
+
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice camera, int error) {
+
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
 
         return  rootView;
     }
 
-    private Camera checkDeviceCamera(){
-        Camera mCamera = null;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private String getBackCameraId(){
+        String[] cameraIds = null;
         try {
-            mCamera = Camera.open();
-        } catch (Exception e) {
+            cameraIds = cameraManager.getCameraIdList();
+        } catch (CameraAccessException e) {
             e.printStackTrace();
         }
-        return mCamera;
-    }
-
-    Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-            if(bitmap==null){
-                Toast.makeText(getContext(), "Captured image is empty", Toast.LENGTH_LONG).show();
-                return;
+        for(String cameraId : cameraIds){
+            CameraCharacteristics characteristics = null;
+            try {
+                characteristics = cameraManager.getCameraCharacteristics(cameraId);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
             }
+            int cameraOrientation = characteristics.get(CameraCharacteristics.LENS_FACING);
+            if(cameraOrientation == CameraCharacteristics.LENS_FACING_BACK) return cameraId;
         }
-    };
-
-    private Bitmap scaleDownBitmapImage(Bitmap bitmap, int newWidth, int newHeight){
-        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
-        return resizedBitmap;
+        return null;
     }
+
 
 }
 
